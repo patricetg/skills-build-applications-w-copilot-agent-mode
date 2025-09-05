60c46138e927c7a48ab8ae345737cd482d9a8916
Ekoue LOGOSU-TEKO <patricelogosuteko@gmail.com>
2025-09-05 17:25:03 +0000

    Added mongodb to backend

---
+++ octofit-tracker/backend/octofit_tracker/management/commands/populate_db.py
@@ -0,0 +1,44 @@
+from django.core.management.base import BaseCommand
+from django.conf import settings
+from django.contrib.auth import get_user_model
+from octofit_tracker.models import Team, Activity, Leaderboard, Workout
+
+class Command(BaseCommand):
+    help = 'Populate the octofit_db database with test data'
+
+    def handle(self, *args, **kwargs):
+        User = get_user_model()
+        # Delete all data
+        User.objects.all().delete()
+        Team.objects.all().delete()
+        Activity.objects.all().delete()
+        Leaderboard.objects.all().delete()
+        Workout.objects.all().delete()
+
+        # Create teams
+        marvel = Team.objects.create(name='Marvel')
+        dc = Team.objects.create(name='DC')
+
+        # Create users
+        users = [
+            User.objects.create_user(username='ironman', email='ironman@marvel.com', password='pass', first_name='Tony', last_name='Stark'),
+            User.objects.create_user(username='spiderman', email='spiderman@marvel.com', password='pass', first_name='Peter', last_name='Parker'),
+            User.objects.create_user(username='batman', email='batman@dc.com', password='pass', first_name='Bruce', last_name='Wayne'),
+            User.objects.create_user(username='wonderwoman', email='wonderwoman@dc.com', password='pass', first_name='Diana', last_name='Prince'),
+        ]
+
+        # Create activities
+        Activity.objects.create(name='Run', user='ironman', team='Marvel')
+        Activity.objects.create(name='Swim', user='spiderman', team='Marvel')
+        Activity.objects.create(name='Cycle', user='batman', team='DC')
+        Activity.objects.create(name='Yoga', user='wonderwoman', team='DC')
+
+        # Create leaderboard
+        Leaderboard.objects.create(team='Marvel', points=200)
+        Leaderboard.objects.create(team='DC', points=180)
+
+        # Create workouts
+        Workout.objects.create(name='Cardio Blast', description='High intensity cardio workout')
+        Workout.objects.create(name='Strength Training', description='Full body strength exercises')
+
+        self.stdout.write(self.style.SUCCESS('octofit_db database populated with test data'))
